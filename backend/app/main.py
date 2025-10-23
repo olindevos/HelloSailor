ec26d7e567a1be2b3c751927d5213a84f6c4902f
olindevos <olindevos@gmail.com>
2025-10-23 16:24:03 +0200

    cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,15 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 app = FastAPI()
+
+# TEMP: sta alle origins toe om de verbinding te testen
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],      # straks beperken we dit
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 @app.get("/healthz")
 def healthz():
